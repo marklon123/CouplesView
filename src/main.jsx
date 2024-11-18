aa0e2c944443e2af11a8398ec94330d62bc4fc5e
Marklon <marklon567@gmail.com>
2024-11-18 14:25:51 -0500

    add _redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
