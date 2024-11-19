32d8b743203eb05b3fa9e8216fcd5fdb861fab52
Marklon <marklon567@gmail.com>
2024-11-19 17:40:23 -0500

    update movies

--- src/components/Movies/PopularMovies.jsx
+++ src/components/Movies/PopularMovies.jsx
@@ -183,8 +183,8 @@ function PopularMovies() {
                 <div className="heading relative">
                     <div className="flex justify-between items-center pt-2 sm:pt-3">
                         <h1 className="trendingHeading text-tertiary text-lg md:text-2xl 2xl:3xl font-medium">Popular Movies</h1>
-                        <a className="cursor-pointer flex items-center nowrap text-sm bg-secondary bg-opacity-80 rounded-full p-[2px] px-2 sm:p-[1px] sm:px-3 md:px-4">
-                            <p className="text-cta text-sm mb-[2px] sm:text-base lg:text-md sm:mb-1">more</p>
+                        <a className="cursor-pointer flex items-center flex-nowrap bg-secondary bg-opacity-80 rounded-full p-[2px] px-2 sm:p-[1px] sm:px-3 md:px-4">
+                            <p className="text-cta text-sm sm:text-base lg:text-md flex items-center transform -translate-y-[10%]">more</p>
                             <ChevronDoubleRightIcon className="ml-1 w-[12px]" />
                         </a>
                     </div>
