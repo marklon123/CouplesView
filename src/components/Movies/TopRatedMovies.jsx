b8c7a9cc46aa7dbd8e6d79bd6447e491a36fe737
Marklon <marklon567@gmail.com>
2024-11-29 17:00:26 -0500

    more link trial fix5

--- src/components/Movies/TopRatedMovies.jsx
+++ src/components/Movies/TopRatedMovies.jsx
@@ -184,7 +184,7 @@ function TopRatedMovies() {
                     <div className="flex justify-between items-center pt-2 sm:pt-3">
                         <h1 className="trendingHeading text-tertiary text-lg md:text-2xl 2xl:3xl font-medium">Top Rated Movies</h1>
                         <a className="flex items-center leading-none cursor-pointer bg-secondary rounded-full bg-opacity-80 p-[2px] px-2 sm:p-[1px] sm:px-3 md:px-4">
-                            <p className="text-cta text-sm md:text-base lg:text-xl">more</p>
+                            <p className="text-cta text-sm md:text-base lg:text-lg">more</p>
                             <ChevronDoubleRightIcon className="ml-1 w-[12px] sm:w-[18px] lg:w-[22px]" />
                         </a>
                     </div>
